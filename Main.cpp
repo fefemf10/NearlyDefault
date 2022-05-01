3a580b693d681af4804cd5c74f38a2112c5622dd
fefemf10 <dima.aldakimov@yandex.ru>
2022-05-02 00:33:24 +0300

    add cmd params, scale, kernel variant, invert add customizable kernels

--- Main.cpp
+++ Main.cpp
@@ -1,35 +1,13 @@
 #include <fstream>
 #include <vector>
+#include <map>
 #include <filesystem>
 #include <string>
 #include <algorithm>
 #include "stb_image.hpp"
 #include "stb_image_write.hpp"
 
-static const std::vector<std::vector<std::vector<uint8_t>>> kernels
-{
-	std::vector<std::vector<uint8_t>>
-	{
-		{ 1, 1 },
-		{ 1, 1 }
-	},
-	{
-		{ 0, 1, 1, 0 },
-		{ 1, 1, 1, 1 },
-		{ 1, 1, 1, 1 },
-		{ 0, 1, 1, 0 }
-	},
-	{
-		{ 0, 0, 1, 1, 1, 1, 0, 0 },
-		{ 0, 1, 1, 1, 1, 1, 1, 0 },
-		{ 1, 1, 1, 1, 1, 1, 1, 1 },
-		{ 1, 1, 1, 1, 1, 1, 1, 1 },
-		{ 1, 1, 1, 1, 1, 1, 1, 1 },
-		{ 1, 1, 1, 1, 1, 1, 1, 1 },
-		{ 0, 1, 1, 1, 1, 1, 1, 0 },
-		{ 0, 0, 1, 1, 1, 1, 0, 0 },
-	}
-};
+std::map<size_t, std::vector<std::vector<std::vector<uint8_t>>>> kernels;
 
 struct Pixel
 {
@@ -39,21 +17,44 @@ struct Pixel
 	unsigned char a;
 };
 
-int main()
+int main(int argc, char* argv[])
 {
 	namespace fs = std::filesystem;
 	stbi_set_flip_vertically_on_load(true);
 	stbi_flip_vertically_on_write(true);
-	fs::copy(fs::current_path() / "input", fs::current_path() / "output", fs::copy_options::overwrite_existing | fs::copy_options::recursive);
-	for (auto& file : fs::recursive_directory_iterator(fs::current_path() / "output"))
+	std::ifstream kernelsFile("kernels.txt");
+	while (!kernelsFile.eof())
 	{
-		if (file.is_regular_file() && file.path().extension() == ".png")
+		int size;
+		kernelsFile >> size;
+		kernels[size].push_back(std::vector<std::vector<uint8_t>>(size, std::vector<uint8_t>(size)));
+		for (size_t i = 0; i < size; i++)
+		{
+			for (size_t j = 0; j < size; j++)
+			{
+				kernelsFile >> (*kernels[size].rbegin())[i][j];
+			}
+		}
+	}
+	int scale{2}, kernelVariant{};
+	bool invert{};
+	if (argc > 1)
+	{
+		scale = std::stoi(argv[1]);
+		kernelVariant = std::stoi(argv[2]);
+		invert = std::stoi(argv[3]);
+	}
+	for (auto& file : fs::recursive_directory_iterator(fs::current_path() / "input"))
+	{
+		if (file.is_regular_file())
 		{
 			Pixel* input{};
 			int width{}, height{}, bpp{};
 			input = reinterpret_cast<Pixel*>(stbi_load(file.path().string().c_str(), &width, &height, &bpp, 4));
-			const int scale = 4;
-			const int cust = 3;
+			
+			const int kernelSize = scale;
+			
+			const int window = scale + 1 + scale % 2;
 			std::vector<Pixel> output(width * height * scale * scale);
 			for (size_t i = 0; i < height; ++i)
 			{
@@ -69,12 +70,12 @@ int main()
 					}
 				}
 			}
-			std::vector<unsigned char> windowr(cust * cust);
-			std::vector<unsigned char> windowg(cust * cust);
-			std::vector<unsigned char> windowb(cust * cust);
-			std::vector<unsigned char> windowa(cust * cust);
-			const int edge = (cust - 1) / 2;
-			const int custq = (cust * cust - 1) / 2;
+			std::vector<unsigned char> windowr(window * window);
+			std::vector<unsigned char> windowg(window * window);
+			std::vector<unsigned char> windowb(window * window);
+			std::vector<unsigned char> windowa(window * window);
+			const int edge = (window - 1) / 2;
+			const int median = (window * window - 1) / 2;
 			for (int i = edge; i < (height * scale) - edge; ++i)
 			{
 				for (int j = edge; j < (width * scale) - edge; ++j)
@@ -82,10 +83,10 @@ int main()
 					int c = 0;
 					for (int fi = -edge; fi <= edge; ++fi)
 					{
-						int dpy = (fi + i) * (width * scale);
+						const int dpy = (fi + i) * (width * scale);
 						for (int fj = -edge; fj <= edge; ++fj)
 						{
-							int dpx = dpy + fj + j;
+							const int dpx = dpy + fj + j;
 							windowr[c] = output[dpx].r;
 							windowg[c] = output[dpx].g;
 							windowb[c] = output[dpx].b;
@@ -97,10 +98,11 @@ int main()
 					std::sort(windowg.begin(), windowg.end());
 					std::sort(windowb.begin(), windowb.end());
 					std::sort(windowa.begin(), windowa.end());
-					output[i * (width * scale) + j].r = windowr[custq];
-					output[i * (width * scale) + j].g = windowg[custq];
-					output[i * (width * scale) + j].b = windowb[custq];
-					output[i * (width * scale) + j].a = windowa[custq];
+					const unsigned int index = i * width * scale + j;
+					output[index].r = windowr[median];
+					output[index].g = windowg[median];
+					output[index].b = windowb[median];
+					output[index].a = windowa[median];
 				}
 			}
 			{
@@ -109,18 +111,21 @@ int main()
 				{
 					for (size_t j = 0; j < (width * scale); ++j)
 					{
-						if (kernels[1][i % scale][j % scale])
+						const unsigned int index = i * width * scale + j;
+						if (kernels[kernelSize][kernelVariant][i % scale][j % scale] == (invert ? '0' : '1'))
 						{
-							output[i * (width * scale) + j] = input[(i / scale * width) + (j / scale)];
+							output[index] = input[(i / scale * width) + (j / scale)];
 						}
-						pixels[(i * (width * scale) + j) * bpp] = output[i * (width * scale) + j].r;
-						pixels[(i * (width * scale) + j) * bpp + 1] = output[i * (width * scale) + j].g;
-						pixels[(i * (width * scale) + j) * bpp + 2] = output[i * (width * scale) + j].b;
+						pixels[index * bpp] = output[index].r;
+						pixels[index * bpp + 1] = output[index].g;
+						pixels[index * bpp + 2] = output[index].b;
 						if (bpp == 4)
-							pixels[(i * (width * scale) + j) * bpp + 3] = output[i * (width * scale) + j].a;
+							pixels[index * bpp + 3] = output[index].a;
 					}
 				}
-				stbi_write_png(file.path().string().c_str(), width * scale, height * scale, bpp, pixels.data(), 0);
+				fs::path tmp = fs::current_path() / "output" / file.path().stem();
+				tmp.replace_extension("png");
+				stbi_write_png(tmp.string().c_str(), width * scale, height * scale, bpp, pixels.data(), 0);
 			}
 			stbi_image_free(input);
 		}
--- stb_image.cpp
+++ stb_image.cpp
@@ -1,4 +1,3 @@
-#define STBI_ONLY_PNG
 #define STBI_NO_FAILURE_STRINGS
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.hpp"
